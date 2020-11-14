5b9bc3c11d0140ebf7fc72835ab91e32cc04dbdc
Alberto Blasco <albertomuni_61@hotmail.com>
2020-11-14 20:20:49 +0100

    Añadida redireccion a las paginas

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ const item2 = document.getElementById('item2');
 const item3 = document.getElementById('item3');
 
 item1.addEventListener('click', (e) => {
-    console.log('hola');
+    setTimeout(() => {
+        window.open("file:///C:/Users/alber/OneDrive/Escritorio/Curso%20Javascript/P01_Alberto/pagina1.html", '_blank');
+    }, 2000);
 });
 item2.addEventListener('click', (e) => {
-    console.log('hola2');
+    setTimeout(() => {
+        window.open("file:///C:/Users/alber/OneDrive/Escritorio/Curso%20Javascript/P01_Alberto/pagina2.html", '_blank');
+    }, 2000);
+    
 });
 item3.addEventListener('click', (e) => {
+    setTimeout(() => {
+        window.open("file:///C:/Users/alber/OneDrive/Escritorio/Curso%20Javascript/P01_Alberto/pagina3.html", '_blank' );
+    }, 2000);
     console.log('hola3');
+    
 });
