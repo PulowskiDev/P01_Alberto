a9180a9855dda79b860a3b94aaf9c72b862618b1
Alberto Blasco <albertomuni_61@hotmail.com>
2020-11-14 20:08:56 +0100

    Añadidas paginas html

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 /**
  * Archico js relacionado con el proyecto P01_Alberto
  */
+const item1 = document.getElementById('item1');
+const item2 = document.getElementById('item2');
+const item3 = document.getElementById('item3');
+
+item1.addEventListener('click', (e) => {
+    console.log('hola');
+});
+item2.addEventListener('click', (e) => {
+    console.log('hola2');
+});
+item3.addEventListener('click', (e) => {
+    console.log('hola3');
+});
